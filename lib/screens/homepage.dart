75b18d55d8a79695342a2716d389377f11ead777
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-07-22 00:22:38 +0530

    Th3_D5_482

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(children: [Text('Hello World')]),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+        ],
+      ),
+    );
+  }
+}
