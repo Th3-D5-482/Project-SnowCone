e92d2dcd370ed72797b3706b580be00fc51d2d40
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-07-22 00:46:44 +0530

    Th3_D5_482

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,22 +1,49 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
+  @override
+  State<Homepage> createState() => HomepageState();
+}
+
+class HomepageState extends State<Homepage> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.all(16.0),
-          child: Column(children: [Text('Hello World')]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Project SnowCone',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.green,
+                  decorationThickness: 5,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
         showUnselectedLabels: false,
+        iconSize: 28,
+        onTap: (value) => setState(() => index = value),
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_add),
+            label: "Library",
+          ),
         ],
       ),
     );
