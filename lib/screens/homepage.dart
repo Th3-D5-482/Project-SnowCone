6a230ea64cdcee4013ded66c87844eb21bf3f3e0
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-07-22 17:57:49 +0530

    Th3_D5_482

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -12,68 +12,18 @@ class HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: Container(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Project SnowCone',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    decoration: TextDecoration.underline,
-                    decorationColor: Colors.green,
-                    decorationThickness: 4,
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 250.0),
-                  alignment: Alignment.center,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Project SnowCone',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.green,
-                            decorationThickness: 4,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 250,
-                          width: 250,
-                          child: Image(
-                            image: AssetImage('assets/images/WIP.png'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: index,
-        showUnselectedLabels: false,
         iconSize: 28,
-        onTap: (value) => setState(() => index = value),
+        onTap: (value) => setState(() {
+          index = value;
+        }),
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
           BottomNavigationBarItem(
             icon: Icon(Icons.library_music),
-            label: "Library",
+            label: 'Library',
           ),
         ],
       ),
