a6977ac0cdc90ad9f936e6e962f14ac4d03da31c
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-07-22 13:42:50 +0530

    Th3_D5_482

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -13,55 +13,52 @@ class HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Project SnowCone',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline,
-                      decorationColor: Colors.green,
-                      decorationThickness: 4,
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Project SnowCone',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Colors.green,
+                    decorationThickness: 4,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 250.0),
+                  alignment: Alignment.center,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Project SnowCone',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.green,
+                            decorationThickness: 4,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 250,
+                          width: 250,
+                          child: Image(
+                            image: AssetImage('assets/images/WIP.png'),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Center(
-                    child: SizedBox(
-                      height: MediaQuery.of(context).size.height - 150,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Project SnowCone',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              decoration: TextDecoration.underline,
-                              decorationColor: Colors.green,
-                              decorationThickness: 4,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 250,
-                            width: 250,
-                            child: Image(
-                              image: AssetImage('assets/images/WIP.png'),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
