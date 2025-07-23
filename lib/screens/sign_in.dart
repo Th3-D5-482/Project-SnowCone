be643653a1ff7b1b31829e1b96f0fa64de9b821f
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-07-23 19:29:28 +0530

    Th3_D5_482

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -12,21 +12,31 @@ class SignIn extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                IconButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  icon: Icon(
-                    Icons.arrow_back_rounded,
-                    color: Colors.white,
-                    size: 30,
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: Icon(
+                      Icons.arrow_back_rounded,
+                      color: Colors.white,
+                      size: 30,
+                    ),
                   ),
                 ),
-                Center(
-                  child: Image(
-                    image: AssetImage('assets/images/logo.png'),
-                    width: 300,
-                    height: 300,
+                Image(
+                  image: AssetImage('assets/images/logo.png'),
+                  width: 280,
+                  height: 280,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Sign In',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey,
                   ),
                 ),
               ],
