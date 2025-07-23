e4d8ec907e2903876fc097e355f27656b49ea2fd
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-07-23 18:19:42 +0530

    Th3_D5_482

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -6,92 +6,95 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image(image: AssetImage('assets/images/welcome.png')),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Project',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(width: 8.0),
-              Text(
-                'SnowCone',
-                style: TextStyle(
-                  fontSize: 32,
-                  color: Colors.blueGrey,
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Image(image: AssetImage('assets/images/welcome.png')),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Project',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Explore your favorite',
-                style: TextStyle(fontSize: 20, color: Colors.grey),
-              ),
-              SizedBox(width: 8.0),
-              Text(
-                'songs',
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.blueGrey,
-                  fontWeight: FontWeight.bold,
+                SizedBox(width: 8.0),
+                Text(
+                  'SnowCone',
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(width: 8.0),
-              Text(
-                'Master every',
-                style: TextStyle(fontSize: 20, color: Colors.grey),
-              ),
-              SizedBox(width: 8.0),
-              Text(
-                'chord',
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.blueGrey,
-                  fontWeight: FontWeight.bold,
+              ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Explore your favorite',
+                  style: TextStyle(fontSize: 20, color: Colors.grey),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(width: 8.0),
-              Text(
-                'Play with',
-                style: TextStyle(fontSize: 20, color: Colors.grey),
-              ),
-              SizedBox(width: 8.0),
-              Text(
-                'confidence',
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.blueGrey,
-                  fontWeight: FontWeight.bold,
+                SizedBox(width: 8.0),
+                Text(
+                  'songs',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(height: 50),
-          ElevatedButton(
-            onPressed: () => {},
-            style: ElevatedButton.styleFrom(fixedSize: Size(350, 50)),
-            child: Text('Get Started'),
-          ),
-        ],
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(width: 8.0),
+                Text(
+                  'Master every',
+                  style: TextStyle(fontSize: 20, color: Colors.grey),
+                ),
+                SizedBox(width: 8.0),
+                Text(
+                  'chord',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(width: 8.0),
+                Text(
+                  'Play with',
+                  style: TextStyle(fontSize: 20, color: Colors.grey),
+                ),
+                SizedBox(width: 8.0),
+                Text(
+                  'confidence',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () => {},
+              style: ElevatedButton.styleFrom(fixedSize: Size(350, 50)),
+              child: Text('Get Started'),
+            ),
+          ],
+        ),
       ),
     );
   }
