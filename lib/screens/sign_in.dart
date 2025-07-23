a153e72d1b112001c432be33e224f0721dac0702
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-07-23 19:15:10 +0530

    Th3_D5_482

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -5,6 +5,35 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  icon: Icon(
+                    Icons.arrow_back_rounded,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+                Center(
+                  child: Image(
+                    image: AssetImage('assets/images/logo.png'),
+                    width: 300,
+                    height: 300,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
