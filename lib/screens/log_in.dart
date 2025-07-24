d4735f2cfc3c67709df4e34dfaab9859428d0b35
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-07-24 16:55:45 +0530

    Th3_D5_482

--- lib/screens/log_in.dart
+++ lib/screens/log_in.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
   const LogIn({super.key});
 
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,13 +51,69 @@ class LogIn extends StatelessWidget {
                 SizedBox(height: 30),
                 TextField(
                   decoration: InputDecoration(
-                    label: Row(
-                      children: [
-                        Icon(Icons.email, color: Colors.white),
-                        SizedBox(width: 8.0),
-                        Text('Email', style: TextStyle(color: Colors.white)),
-                      ],
+                    label: Text('Email', style: TextStyle(color: Colors.white)),
+                    hintText: 'Enter your email',
+                    prefixIcon: Icon(Icons.email, color: Colors.white54),
+                  ),
+                  autocorrect: false,
+                  autofillHints: [AutofillHints.email],
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    label: Text(
+                      'Password',
+                      style: TextStyle(color: Colors.white),
                     ),
+                    hintText: 'Enter your password',
+                    prefixIcon: Icon(Icons.lock, color: Colors.white54),
+                    suffixIcon: Icon(Icons.visibility, color: Colors.white54),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: isChecked,
+                      onChanged: (value) => setState(() {
+                        isChecked = value ?? false;
+                      }),
+                      activeColor: Colors.blueGrey,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      side: BorderSide(color: Colors.white54, width: 1.5),
+                    ),
+                    Text(
+                      'Remember me',
+                      style: TextStyle(color: Colors.blueGrey, fontSize: 16),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(350, 50),
+                    backgroundColor: Colors.blueGrey,
+                    foregroundColor: Colors.white,
+                    textStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                  ),
+                  child: Text('Log In'),
+                ),
+                SizedBox(height: 8),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(color: Colors.blueGrey, fontSize: 16),
                   ),
                 ),
               ],
