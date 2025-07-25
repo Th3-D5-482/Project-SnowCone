77bb286a105b8a1490e4a600e523c0d85747aa69
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-07-25 21:26:35 +0530

    Th3_D5_482

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -17,112 +17,270 @@ class Welcome extends StatelessWidget {
       child: Scaffold(
         body: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              Image(image: AssetImage('assets/images/welcome.png')),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Project',
-                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          child: kIsWeb
+              ? Padding(
+                  padding: const EdgeInsets.only(
+                    top: 100.0,
+                    bottom: 24,
+                    left: 24,
+                    right: 24,
                   ),
-                  SizedBox(width: 8.0),
-                  Text(
-                    'SnowCone',
-                    style: TextStyle(
-                      fontSize: 32,
-                      color: Colors.blueGrey,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Explore your favorite',
-                    style: TextStyle(fontSize: 20, color: Colors.grey),
-                  ),
-                  SizedBox(width: 8.0),
-                  Text(
-                    'songs',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Colors.blueGrey,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(width: 8.0),
-                  Text(
-                    'Master every',
-                    style: TextStyle(fontSize: 20, color: Colors.grey),
-                  ),
-                  SizedBox(width: 8.0),
-                  Text(
-                    'chord',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Colors.blueGrey,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(width: 8.0),
-                  Text(
-                    'Play with',
-                    style: TextStyle(fontSize: 20, color: Colors.grey),
-                  ),
-                  SizedBox(width: 8.0),
-                  Text(
-                    'confidence',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Colors.blueGrey,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 30),
-              Padding(
-                padding: kIsWeb
-                    ? const EdgeInsets.only(bottom: 24)
-                    : const EdgeInsets.only(bottom: 16),
-                child: ElevatedButton(
-                  onPressed: () => {
-                    Navigator.of(context).push(
-                      PageRouteBuilder(
-                        pageBuilder: (context, animation, secondaryAnimation) =>
-                            const SignIn(),
-                        transitionDuration: Duration(milliseconds: 100),
-                        transitionsBuilder:
-                            (context, animation, secondaryAnimation, child) =>
-                                FadeTransition(
-                                  opacity: animation,
-                                  child: child,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage('assets/images/welcome1.png'),
+                        width: 600,
+                        height: 600,
+                      ),
+                      SizedBox(width: 50),
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Project',
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
                                 ),
+                              ),
+                              SizedBox(width: 8.0),
+                              Text(
+                                'SnowCone',
+                                style: TextStyle(
+                                  fontSize: 36,
+                                  color: Colors.blueGrey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 100),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '" Explore your favorite',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              SizedBox(width: 8.0),
+                              Text(
+                                'songs',
+                                style: TextStyle(
+                                  fontSize: 26,
+                                  color: Colors.blueGrey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(width: 8.0),
+                              Text(
+                                'Master every',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              SizedBox(width: 8.0),
+                              Text(
+                                'chord',
+                                style: TextStyle(
+                                  fontSize: 26,
+                                  color: Colors.blueGrey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(width: 8.0),
+                              Text(
+                                'Play with',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              SizedBox(width: 8.0),
+                              Text(
+                                'confidence "',
+                                style: TextStyle(
+                                  fontSize: 26,
+                                  color: Colors.blueGrey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 100),
+                          Padding(
+                            padding: kIsWeb
+                                ? const EdgeInsets.only(bottom: 24)
+                                : const EdgeInsets.only(bottom: 16),
+                            child: ElevatedButton(
+                              onPressed: () => {
+                                Navigator.of(context).push(
+                                  PageRouteBuilder(
+                                    pageBuilder:
+                                        (
+                                          context,
+                                          animation,
+                                          secondaryAnimation,
+                                        ) => const SignIn(),
+                                    transitionDuration: Duration(
+                                      milliseconds: 100,
+                                    ),
+                                    transitionsBuilder:
+                                        (
+                                          context,
+                                          animation,
+                                          secondaryAnimation,
+                                          child,
+                                        ) => FadeTransition(
+                                          opacity: animation,
+                                          child: child,
+                                        ),
+                                  ),
+                                ),
+                              },
+                              style: ElevatedButton.styleFrom(
+                                fixedSize: Size(350, 50),
+                              ),
+                              child: Text('Get Started'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              : Column(
+                  children: [
+                    Image(image: AssetImage('assets/images/welcome.png')),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Project',
+                          style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(width: 8.0),
+                        Text(
+                          'SnowCone',
+                          style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 80),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Explore your favorite',
+                          style: TextStyle(fontSize: 24, color: Colors.grey),
+                        ),
+                        SizedBox(width: 8.0),
+                        Text(
+                          'songs',
+                          style: TextStyle(
+                            fontSize: 26,
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(width: 8.0),
+                        Text(
+                          'Master every',
+                          style: TextStyle(fontSize: 24, color: Colors.grey),
+                        ),
+                        SizedBox(width: 8.0),
+                        Text(
+                          'chord',
+                          style: TextStyle(
+                            fontSize: 26,
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(width: 8.0),
+                        Text(
+                          'Play with',
+                          style: TextStyle(fontSize: 24, color: Colors.grey),
+                        ),
+                        SizedBox(width: 8.0),
+                        Text(
+                          'confidence',
+                          style: TextStyle(
+                            fontSize: 26,
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 80),
+                    Padding(
+                      padding: kIsWeb
+                          ? const EdgeInsets.only(bottom: 24)
+                          : const EdgeInsets.only(bottom: 16),
+                      child: ElevatedButton(
+                        onPressed: () => {
+                          Navigator.of(context).push(
+                            PageRouteBuilder(
+                              pageBuilder:
+                                  (context, animation, secondaryAnimation) =>
+                                      const SignIn(),
+                              transitionDuration: Duration(milliseconds: 100),
+                              transitionsBuilder:
+                                  (
+                                    context,
+                                    animation,
+                                    secondaryAnimation,
+                                    child,
+                                  ) => FadeTransition(
+                                    opacity: animation,
+                                    child: child,
+                                  ),
+                            ),
+                          ),
+                        },
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: Size(350, 50),
+                        ),
+                        child: Text('Get Started'),
                       ),
                     ),
-                  },
-                  style: ElevatedButton.styleFrom(fixedSize: Size(350, 50)),
-                  child: Text('Get Started'),
+                  ],
                 ),
-              ),
-            ],
-          ),
         ),
       ),
     );
