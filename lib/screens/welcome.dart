6ad2a2c8e7ade39177e5688da8df9283de338aff
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-07-26 13:02:50 +0530

    Th3_D5_482

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -42,7 +42,7 @@ class Welcome extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 30),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -99,7 +99,7 @@ class Welcome extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(height: 30),
+              SizedBox(height: 40),
               Padding(
                 padding: kIsWeb
                     ? const EdgeInsets.only(bottom: 24)
