33e7098dd047c374bed598400b19216e6303b41a
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-08-18 02:38:26 +0530

    Th3_D5_482

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -55,14 +55,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "84f4ee706fc9b2a283376b21fa6373e8
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "cc70ae2917b1ab99966486035872ca5a",
+"flutter_bootstrap.js": "a063f7f39e1acd968413d8ccca03f388",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "6050ecd400596b6ec56121843dc6e750",
 "/": "6050ecd400596b6ec56121843dc6e750",
-"main.dart.js": "eeae7db510d71c9e4761c33e8fcd876d",
+"main.dart.js": "679e4973d962b2a3bb11fb5e87efd67a",
 "manifest.json": "6858a767bc61f44ddcf96ba3905fea09",
 "version.json": "794af03ad599eb48d29243c1d69873a8"};
 // The application shell files that are downloaded before a service worker can
--- lib/screens/disclaimer.dart
+++ lib/screens/disclaimer.dart
@@ -12,7 +12,7 @@ class _DisclaimerState extends State<Disclaimer> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 5), () {
       // ignore: use_build_context_synchronously
       Navigator.of(context).push(
         PageRouteBuilder(
