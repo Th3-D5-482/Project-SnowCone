9cce86dc7c6fb4bc530b74a8bf2e38b512f0982c
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-09-04 02:33:05 +0530

    Th3_D5_482

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -58,7 +58,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a6903670f04147d3cd78574d7ee4d51e
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d936328de4329927914e53fa8486ec3a",
+"flutter_bootstrap.js": "376443e4f39fd061108b08779ecb204c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
--- lib/screens/offline_page.dart
+++ lib/screens/offline_page.dart
@@ -28,14 +28,14 @@ class OfflinePage extends StatelessWidget {
                           'assets/images/random/no_wifi.png',
                         ),
                         backgroundColor: Colors.transparent,
-                        radius: 110,
+                        radius: 90,
                       ),
                       SizedBox(height: 20),
                       Text(
                         'Sorry mate, you\'re offline!',
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: 22,
+                          fontSize: 18,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
