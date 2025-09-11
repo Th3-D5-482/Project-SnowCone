2f98c6634e4a5a7bc6f843d70f2767a9ca301ce3
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-09-11 10:53:49 +0530

    Th3_D5_482

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -63,7 +63,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2033fb67b33300dcc34e864e9a8fa3ba
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a5df517d0097c0d40ac34d5e997408bf",
+"flutter_bootstrap.js": "af8e9d1a9f8c290868baa923b69cdfd5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
