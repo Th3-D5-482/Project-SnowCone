4f13c7d78831166d2812cc962030c317508cba5b
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-10-09 16:26:00 +0530

    Th3_D5_482

--- lib/screens/songs_list.dart
+++ lib/screens/songs_list.dart
@@ -22,72 +22,85 @@ class _SongsListState extends State<SongsList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              double screenWidth = constraints.maxWidth;
-              bool isDesktop =
-                  (kIsWeb || Platform.isWindows) && screenWidth > 1000;
-              double horizontalPadding = isDesktop ? 200 : 16;
-              return Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: horizontalPadding,
-                  vertical: kIsWeb ? 16 : 8,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: IconButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        icon: Icon(
-                          Icons.arrow_back_rounded,
-                          color: Colors.grey,
-                          size: 28,
-                        ),
+      body: SingleChildScrollView(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            double screenWidth = constraints.maxWidth;
+            bool isDesktop =
+                (kIsWeb || Platform.isWindows) && screenWidth > 1000;
+            double horizontalPadding = isDesktop ? 200 : 0;
+            return Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: horizontalPadding,
+                vertical: kIsWeb ? 16 : 0,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color.fromRGBO(255, 235, 190, 0.0),
+                          Color(0xFFDDE2EA),
+                          Color.fromRGBO(255, 235, 190, 0.0),
+                        ],
+                        stops: [0.0, 0.5, 1.0],
                       ),
                     ),
-                    SizedBox(height: 20),
-                    widget.isBand
-                        ? Align(
-                            alignment: Alignment.center,
-                            child: CircleAvatar(
-                              radius: 115,
-                              backgroundImage: NetworkImage(widget.imageName),
-                            ),
-                          )
-                        : Align(
-                            alignment: Alignment.center,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Image.network(
-                                widget.imageName,
-                                width: 230,
-                                height: 230,
+                    child: SizedBox(
+                      height: 490,
+                      child: Column(
+                        children: [
+                          SizedBox(height: 40),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: IconButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              icon: Icon(
+                                Icons.arrow_back_rounded,
+                                color: Colors.grey,
+                                size: 28,
                               ),
                             ),
                           ),
-                    SizedBox(height: 20),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        widget.songTitle,
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
+                          SizedBox(height: 20),
+                          widget.isBand
+                              ? CircleAvatar(
+                                  radius: 115,
+                                  backgroundImage: NetworkImage(
+                                    widget.imageName,
+                                  ),
+                                )
+                              : ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: Image.network(
+                                    widget.imageName,
+                                    width: 230,
+                                    height: 230,
+                                  ),
+                                ),
+                          SizedBox(height: 20),
+                          Text(
+                            widget.songTitle,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
-              );
-            },
-          ),
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
