3107aadce809df981903a9a3b7b7aca005e06500
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-10-23 02:06:27 +0530

    Th3_D5_482

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:snowcone/database/database.dart';
-import 'package:snowcone/screens/library_page.dart';
 import 'package:snowcone/screens/search_page.dart';
 import 'package:snowcone/screens/songs_list.dart';
 
@@ -27,10 +26,7 @@ class _HomePageState extends State<HomePage> {
         return Future.value(false);
       },
       child: Scaffold(
-        body: IndexedStack(
-          index: index,
-          children: [HomeView(), SearchPage(), LibraryPage()],
-        ),
+        body: IndexedStack(index: index, children: [HomeView(), SearchPage()]),
         bottomNavigationBar: BottomNavigationBar(
           backgroundColor: Colors.transparent,
           currentIndex: index,
@@ -43,10 +39,6 @@ class _HomePageState extends State<HomePage> {
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
             BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.library_music),
-              label: 'Library',
-            ),
           ],
         ),
       ),
